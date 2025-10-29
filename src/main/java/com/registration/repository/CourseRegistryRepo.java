247c9e4e4c3dae9e3af6a7617fc48063b3291773
HABEEB RAHMAN J <127578663+habeebrahman02@users.noreply.github.com>
2025-10-29 11:04:23 +0530

    Delete src directory

--- src/main/java/com/registration/repository/CourseRegistryRepo.java
+++
@@ -1,9 +0,0 @@
-package com.registration.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.registration.model.CourseRegistry;
-
-@Repository
-public interface CourseRegistryRepo extends JpaRepository<CourseRegistry, Long> {
-}
--- src/main/java/com/registration/repository/CourseRepo.java
+++
@@ -1,9 +0,0 @@
-package com.registration.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.registration.model.Course;
-
-@Repository
-public interface CourseRepo extends JpaRepository<Course, Long> {
-}
